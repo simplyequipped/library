1526d919d77cf0fcaafdbf3c71d0b5b3f779fecc
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2023-12-21 15:45:59 -0500

    Update start_library.py

--- start_library.py
+++ start_library.py
@@ -1,55 +1,11 @@
+import time
+import argparse
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from socketserver import ThreadingMixIn
-
-import json
-import time
-import urllib
-import argparse
 
 # local imports
 import services as _services
 import signals as _signals
-
-
-class LibraryRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        global services
-        parsed_path = urllib.parse.urlparse(self.path)
-        path = parsed_path.path
-        params = urllib.parse.parse_qs(parsed_path.query)
-        service = path.strip(' /')
-
-        if service in services:
-            self.send_response(302)  # temporary redirect
-            self.send_header('Location', services[service].get_url())
-            self.end_headers()
-        else:
-            self.send_response(404)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write(b'404 - Not Found')
-
-    def do_POST(self):
-        global signals
-        # parse post data
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length).decode('utf-8')
-        data = json.loads(post_data)
-
-        # process signal data
-        if 'signal' in data and data['signal'] in signals:
-            response = signals.parse(data)
-
-        # respond to signal
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        response_data = json.dumps(response)
-        self.wfile.write( response_data.encode('utf-8') )
-
-
-class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    daemon_threads = True
 
 
 if __name__ == '__main__':    
