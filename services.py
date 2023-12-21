72036ee28df2875b82206b1fa0749a050b51230c
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2023-12-21 15:45:56 -0500

    Update services.py

--- services.py
+++ services.py
@@ -1,7 +1,50 @@
 import os
+import json
+import urllib
 import shelex
 import platform
 import subprocess
+
+
+class LibraryRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        global services
+        parsed_path = urllib.parse.urlparse(self.path)
+        path = parsed_path.path
+        params = urllib.parse.parse_qs(parsed_path.query)
+        service = path.strip(' /')
+
+        if service in services:
+            self.send_response(302)  # temporary redirect
+            self.send_header('Location', services[service].get_url())
+            self.end_headers()
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'404 - Not Found')
+
+    def do_POST(self):
+        global signals
+        # parse post data
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode('utf-8')
+        data = json.loads(post_data)
+
+        # process signal data
+        if 'signal' in data and data['signal'] in signals:
+            response = signals.parse(data)
+
+        # respond to signal
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        response_data = json.dumps(response)
+        self.wfile.write( response_data.encode('utf-8') )
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    daemon_threads = True
 
 
 class Service:
