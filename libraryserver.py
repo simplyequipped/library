8606e9cf4aef539c03b9def0eb0203fe63e9068e
Simply Equipped <92337033+simplyequipped@users.noreply.github.com>
2023-12-22 09:28:06 -0500

    path handling for signal requests

--- libraryserver.py
+++ libraryserver.py
@@ -10,6 +10,7 @@ import signals
 
 class LibraryRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        # parse request path
         parsed_path = urllib.parse.urlparse(self.path)
         path = parsed_path.path
         params = urllib.parse.parse_qs(parsed_path.query)
@@ -26,21 +27,25 @@ class LibraryRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(b'404 - Not Found')
 
     def do_POST(self):
+        # parse request path
+        parsed_path = urllib.parse.urlparse(self.path)
+        path = parsed_path.path
         # parse post data
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length).decode('utf-8')
         data = json.loads(post_data)
 
-        # process signal data
-        if 'signal' in data and data['signal'] in self.server.signals:
-            response = self.server.signals.parse(data)
-
-        # respond to signal
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        response_data = json.dumps(response)
-        self.wfile.write( response_data.encode('utf-8') )
+        if path == '/signals':
+            # process signal data
+            if 'signal' in data and data['signal'] in self.server.signals:
+                response = self.server.signals.parse(data)
+    
+            # respond to signal
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            response_data = json.dumps(response)
+            self.wfile.write( response_data.encode('utf-8') )
 
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
